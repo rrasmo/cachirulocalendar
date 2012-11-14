63dda6213a583de6d2afb0c36325a7b9567c440f
Rafael Ramos <rrasmo@gmail.com>
2012-11-14 14:50:41 +0100

    Added models Community and Event, setup instructions

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ CachiruloCalendar
 
 A calendar to display events and activities organized by diverse technology groups or communities in Aragón in a centralized way. One calendar to rule them all.
 
+Setup
+-----
+
+    git clone
+    cd cachirulocalendar/cachirulocalendar
+    #assumes a mysql server in localhost with root account with no password, see settings.py
+    mysqladmin -u root create cachirulocalendar
+    python manage.py syncdb
+    python manage.py runserver
+
--- cachirulocalendar/events/models.py
+++ cachirulocalendar/events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, null=True)
+    url = models.CharField(max_length=200, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, null=True)
+    url = models.CharField(max_length=200, null=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    pub_date = models.DateTimeField(null=True)
+    place = models.CharField(max_length=200, null=True)
+    community = models.ForeignKey(Community)
+
+    def __unicode__(self):
+        return self.name
+
