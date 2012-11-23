7b344aafb0b2890a3b5ec4edbc72d6d5268dabb5
Rafael Ramos <rrasmo@gmail.com>
2012-11-24 00:06:31 +0100

    Home view shows list of events

--- cachirulocalendar/cachirulocalendar/urls.py
+++ cachirulocalendar/cachirulocalendar/urls.py
@@ -4,14 +4,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cachirulocalendar.views.home', name='home'),
-    # url(r'^cachirulocalendar/', include('cachirulocalendar.foo.urls')),
+urlpatterns = patterns('events.views',
+    url(r'^$', 'home', name='home'),
+)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
+
--- cachirulocalendar/events/views.py
+++ cachirulocalendar/events/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from events.models import Event
+
+
+def home(request):
+    events = Event.objects.all().order_by('start_date')
+    return render_to_response('events/index.html', {'events': events})
