c8318b3b6ebc56154e249e4155f0476415e273c5
Rafael Ramos <rrasmo@gmail.com>
2013-02-06 12:07:21 +0100

    Form to filter events by community

--- cachirulocalendar/events/views.py
+++ cachirulocalendar/events/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render_to_response
-from events.models import Event
+from django import forms
+from django.template import RequestContext
+from events.models import Event, Community
 
 
 def home(request):
-    events = Event.objects.all().order_by('start_date')
-    return render_to_response('events/index.html', {'events': events})
+
+    class CommunitiesForm(forms.Form):
+        select = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple)
+
+    communities = Community.objects.all().order_by('name')
+    choices = map(lambda c: (c.id, c.name), communities)
+    ids = map(lambda c: c.id, communities)
+
+    if request.method == 'POST':
+        form = CommunitiesForm(request.POST)
+        form.fields['select'].choices = choices
+        if form.is_valid():
+            selected = form.cleaned_data['select']
+    else:
+        form = CommunitiesForm()
+        form.fields['select'].choices = choices
+        form.fields['select'].initial = ids
+        selected = ids
+
+    events = Event.objects.filter(community_id__in=selected).order_by('start_date')
+
+    return render_to_response('events/index.html', {'events': events, 'form': form}, context_instance = RequestContext(request))
+
